f287fac7f349119065730422128907e600eae9f4
retamal_f <javier.retamal.fontannaz@gmail.com>
2023-05-01 22:51:14 -0400

    Creacion de repo e inicializacion

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+function App() {
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+      <Text>Digimon App</Text>
+    </View>
+  );
+
+}
+
+
+
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
